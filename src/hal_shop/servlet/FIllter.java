f3c1721eda0d64477531580e48cbcb98429bfb6d
ArakiTakaki <araki_takaki@yahoo.co.jp>
2018-09-10 07:52:31 +0900

    index.htmlへ強制リダイレクトさせる。

--- src/hal_shop/servlet/FIllter.java
+++ src/hal_shop/servlet/FIllter.java
@@ -1,26 +1,28 @@
 package hal_shop.servlet;
 
 import java.io.IOException;
+import java.util.Objects;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 
 /**
- * Servlet Filter implementation class FIllter
+ * Servlet Filter implementation class Fillter
  */
 @WebFilter("/*")
-public class FIllter implements Filter {
-	private static final String CHARSET = "utf-8";
-    
+public class Fillter implements Filter {
 
     /**
      * Default constructor. 
      */
-    public FIllter() {
+    public Fillter() {
         // TODO Auto-generated constructor stub
     }
 
@@ -35,7 +37,15 @@ public class FIllter implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		request.setCharacterEncoding(CHARSET);
+		HttpServletRequest req = (HttpServletRequest)request;
+		
+		//API以外は弾いてindex.htmlへリダイレクトさせる。
+		String[] URI = req.getRequestURI().split("/", -1);
+		if(!Objects.equals(URI[2], "API")) {
+			System.out.println("test");
+			request.getRequestDispatcher("index.html").forward(request, response);
+			return;
+		}
 		chain.doFilter(request, response);
 	}
 
